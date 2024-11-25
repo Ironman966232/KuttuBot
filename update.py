54f3f3b217a1eedc52e590b266d9c010d79bb8dc
Ironman966232 <ironman966232@gmail.com>
2024-11-25 12:13:41 +0530

    Update update.py

--- update.py
+++ update.py
@@ -40,8 +40,8 @@ if UPSTREAM_REPO is not None:
         srun(["rm", "-rf", ".git"])
 
     update = srun([f"git init -q \
-                     && git config --global user.email doc.adhikari@gmail.com \
-                     && git config --global user.name weebzone \
+                     && git config --global user.email doc.ironman966232@gmail.com \
+                     && git config --global user.name Ironman966232 \
                      && git add . \
                      && git commit -sm update -q \
                      && git remote add origin {UPSTREAM_REPO} \
